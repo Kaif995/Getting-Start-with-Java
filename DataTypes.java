588d3c5a036db35b4c4e08ebf10aad394586b934
Mohammed Kaif Shamim <kaifshamim995@gmail.com>
2025-11-02 18:46:06 +0500

    Add DataTypes class with basic variable examples

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Ali";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
