e396b947bace00634fa80f7589dea54a9874d322
Mohammed Kaif Shamim <kaifshamim995@gmail.com>
2025-11-02 18:52:19 +0500

    Remove author name from output

---
+++ stringMeth.java
@@ -0,0 +1,13 @@
+public class stringMeth {
+    public static void main(String[] args) {
+        String text = " Hello World! ";
+
+        System.out.println("Original: '" + text + "'");
+        System.out.println("Lowercase: " + text.toLowerCase());
+        System.out.println("Uppercase: " + text.toUpperCase());
+        System.out.println("Replaced: " + text.replace("World", "Java"));
+        System.out.println("Length: " + text.length());
+        System.out.println("Character at index 1: " + text.charAt(1));
+      
+    }
+}
