5e8cead7d7c57cf400084258a3d730267c7eb3c6
Mohammed Kaif Shamim <kaifshamim995@gmail.com>
2025-11-12 23:10:23 +0500

    Add Main_private class with main method

---
+++ Main_private.java
@@ -0,0 +1,13 @@
+package kaif_lab5;
+
+public class Main_private {
+
+	public static void main(String[] args) {
+		Private_acc p1=new Private_acc();
+		p1.setid(0321);
+		System.out.println(p1.getid());
+		
+
+	}
+
+}
